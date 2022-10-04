b40b34af44f9505d2097f6c174a63b31c59db8b1
Logkesh <logkeshhemanth@gmail.com>
2022-10-04 14:48:55 +0530

    Sorter Updated

--- SortAlgos/Sorter.py
+++ SortAlgos/Sorter.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from bucketsort import BucketSort
 from quicksort import QuickSort
 
@@ -7,17 +8,28 @@ def GenerateAr(size = 10):
         return array
 
 def Print_Sorting(array):
-    func = [BucketSort,QuickSort]
-    for i in func:
-        print("Before Sorting...")
-        print(array)
-        res = i(array)
-        print("After Sorting...")
-        print(res)
+    func = {"BucketSort":BucketSort,"QuickSort":QuickSort}
+    for i in func.keys():
+        print(i," with ",len(array))
+        title = i + " with " + str(len(array)) + " Elements"
+        print("Sorting Array...")
+        PlotGraph(array,title+" Before Sorting")
+        res = func[i](array)
+        print("Array Sorted...")
+        PlotGraph(res,title+" After Sorting")
+
+def PlotGraph(array,title):
+    plt.xlabel("Range")
+    plt.ylabel("Elements")
+    plt.title(title)
+    plt.plot(range(len(array)),array)
+    plt.show()
 
 def main():
-    array = GenerateAr()
-    Print_Sorting(array)
+    for i in range(1,6):
+        n = 10**i    
+        array = GenerateAr(n)
+        Print_Sorting(array)
     
 if __name__ == "__main__":
     main()
